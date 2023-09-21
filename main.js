7e9863dbc0ef3406fbf26f8d6824203fb9df2146
Diego <Diego>
2023-09-21 17:21:14 +0200

    up

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("hola")
+function searchMovie(){
+    const nameMovie = document.getElementById("input").value
+    fetch('http://www.omdbapi.com/?t'+nameMovie , {
+        method : "get",
+    }).then(async (response)=>{
+        var data = await response.json();
+    })
+    
+}
