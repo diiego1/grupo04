ee468b67ad19a92033e8439bc36037e5dec197a8
Diego <Diego>
2023-09-21 17:24:02 +0200

    up

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function searchMovie(){
     const nameMovie = document.getElementById("input").value
-    fetch('http://www.omdbapi.com/?t='+nameMovie , {
+    fetch('http://www.omdbapi.com/?apikey=aa4591de&t='+nameMovie , {
         method : "get",
     }).then(async (response)=>{
         var data = await response.json();
